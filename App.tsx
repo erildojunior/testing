3b6e39ea60b3f3b0bfc47cbee4b73159393eac64
erildojunior <erildopajunior@gmail.com>
2022-05-24 12:46:09 -0300

    teste

--- App.tsx
+++ App.tsx
@@ -29,7 +29,7 @@ import {
 
 const Section: React.FC<{
   title: string;
-}> = ({children, title}) => {
+}> = ({ children, title }) => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -75,7 +75,7 @@ const App = () => {
           }}>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+            screen and then come back to see your edits. lba
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
